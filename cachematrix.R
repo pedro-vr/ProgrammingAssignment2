bbf0e6b71255f6aa126c72858af84d0367f823fe
Pedro Velázquez <pedro_vela_07@hotmail.com>
2020-04-28 10:32:11 -0500

    version1.1

--- cachematrix.R
+++ cachematrix.R
@@ -26,8 +26,6 @@
 # 2.  get the value of the vector
 # 3.  set the value of the mean
 # 4.  get the value of the mean
-
-<!-- -->
   
   makeVector <- function(x = numeric()) {
     m <- NULL
@@ -86,29 +84,6 @@ cachemean <- function(x, ...) {
 # For this assignment, assume that the matrix supplied is always
 # invertible.
 # 
-# In order to complete this assignment, you must do the following:
-#   
-#   1.  Fork the GitHub repository containing the stub R files at
-# [https://github.com/rdpeng/ProgrammingAssignment2](https://github.com/rdpeng/ProgrammingAssignment2)
-# to create a copy under your own account.
-# 2.  Clone your forked GitHub repository to your computer so that you can
-# edit the files locally on your own machine.
-# 3.  Edit the R file contained in the git repository and place your
-# solution in that file (please do not rename the file).
-# 4.  Commit your completed R file into YOUR git repository and push your
-# git branch to the GitHub repository under your account.
-# 5.  Submit to Coursera the URL to your GitHub repository that contains
-# the completed R code for the assignment.
-
-### Grading
-
-# This assignment will be graded via peer assessment.
-
-
-## R Programming Project 2
-### Submitted another assignment for this, but cannot find it so used the one below for complete documentation of specialization
-### [UtkarshPathrabe](https://github.com/UtkarshPathrabe/R-Programming-Johns-Hopkins-Bloomberg-School-of-Public-Health-Coursera/blob/master/programming-assignment-2/cachematrix.R)
-```
 ## A pair of functions that cache the inverse of a matrix
 
 
